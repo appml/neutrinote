1b5187d35b7fa4be94b4f76431949e90891cd6b2
saelim <saelim@gmail.com>
2024-08-29 21:51:42 -0400

    Support keyboard fragment closing event

--- app/src/main/java/com/appmindlab/nano/DisplayDBEntry.java
+++ app/src/main/java/com/appmindlab/nano/DisplayDBEntry.java
@@ -3481,20 +3481,27 @@ public class DisplayDBEntry extends AppCompatActivity implements PopupMenu.OnMen
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK) {
-            Log.d(Const.TAG, "nano -- onKeyDown");
+        // testing
+        Log.d(Const.TAG, "nano -- onKeyDown, keyCode: " + keyCode);
 
-            // Ignore if in immersive mode
-            if (mImmersiveMode) {
-                exitImmersiveMode();
-                return false;
-            }
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_BACK:
+                // Ignore if in immersive mode
+                if (mImmersiveMode) {
+                    exitImmersiveMode();
+                    return false;
+                }
+                return true;
+            default:
+                return super.onKeyDown(keyCode, event);
         }
-        return super.onKeyDown(keyCode, event);
     }
 
     @Override
     public boolean onKeyUp(int keyCode, KeyEvent event) {
+        // testing
+        Log.d(Const.TAG, "nano -- onKeyUp, keyCode: " + keyCode);
+
         switch (keyCode) {
             case KeyEvent.KEYCODE_D:
                 if (event.isCtrlPressed())
@@ -3572,6 +3579,16 @@ public class DisplayDBEntry extends AppCompatActivity implements PopupMenu.OnMen
             case KeyEvent.KEYCODE_MINUS:
                 if (event.isCtrlPressed())
                     scaleFontSize(Const.SCALE_FACTOR_DECREMENT, mContent.getSelectionStart());
+                return true;
+            case KeyEvent.KEYCODE_DEL:
+                // Close the top fragment if more than 1 fragment active
+                if (event.isCtrlPressed()) {
+                    if ((getSupportFragmentManager().getBackStackEntryCount() > 1)) {
+                        closeTopFragment();
+                        mContent.requestFocus();
+                        return false;
+                    }
+                }
                 return true;
             default:
                 return super.onKeyUp(keyCode, event);
